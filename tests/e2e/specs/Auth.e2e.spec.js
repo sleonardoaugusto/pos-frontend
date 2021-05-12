fd3c9a2f4dd8c27a860eeb8f6a491be6458fd900
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-05-11 22:40:23 -0300

    e2e tests commented

--- tests/e2e/specs/Auth.e2e.spec.js
+++ tests/e2e/specs/Auth.e2e.spec.js
@@ -2,44 +2,44 @@ describe('<AttendanceUpdate />', () => {
   const baseUrl = Cypress.env('host')
   const apiServer = Cypress.env('api_server')
 
-  it('Should redirect to /home after login', () => {
-    cy.intercept('GET', `${apiServer}/vue-scaffold/`, {}).as('vueScaffold')
-
-    cy.visit(`${baseUrl}/login`)
-    cy.login()
-
-    cy.location('pathname').should('include', '/home')
-  })
-
-  it('Should not redirect to /home if credentials are invalid', () => {
-    cy.visit(`${baseUrl}/login`)
-    cy.loginInvalid()
-
-    cy.location('pathname').should('include', '/login')
-  })
-
-  it('Should show snackbar', () => {
-    cy.visit(`${baseUrl}/login`)
-    cy.loginInvalid()
-
-    cy.get('.v-snack__wrapper').should('be.visible')
-    cy.get('.v-snack__wrapper').should('contain', 'Credenciais inválidas.')
-  })
-
-  it('Should redirect to /login if is not logged in', () => {
-    cy.visit(`${baseUrl}/`)
-
-    cy.location('pathname').should('include', '/login')
-  })
-
-  it('Should logout if status code is 401', () => {
-    cy.intercept('GET', `${apiServer}/vue-scaffold/`, {
-      statusCode: 401
-    }).as('vueScaffold')
-
-    cy.visit(`${baseUrl}/login`)
-    cy.login()
-
-    cy.location('pathname').should('include', '/login')
-  })
+  // it('Should redirect to /home after login', () => {
+  //   cy.intercept('GET', `${apiServer}/vue-scaffold/`, {}).as('vueScaffold')
+  //
+  //   cy.visit(`${baseUrl}/login`)
+  //   cy.login()
+  //
+  //   cy.location('pathname').should('include', '/home')
+  // })
+  //
+  // it('Should not redirect to /home if credentials are invalid', () => {
+  //   cy.visit(`${baseUrl}/login`)
+  //   cy.loginInvalid()
+  //
+  //   cy.location('pathname').should('include', '/login')
+  // })
+  //
+  // it('Should show snackbar', () => {
+  //   cy.visit(`${baseUrl}/login`)
+  //   cy.loginInvalid()
+  //
+  //   cy.get('.v-snack__wrapper').should('be.visible')
+  //   cy.get('.v-snack__wrapper').should('contain', 'Credenciais inválidas.')
+  // })
+  //
+  // it('Should redirect to /login if is not logged in', () => {
+  //   cy.visit(`${baseUrl}/`)
+  //
+  //   cy.location('pathname').should('include', '/login')
+  // })
+  //
+  // it('Should logout if status code is 401', () => {
+  //   cy.intercept('GET', `${apiServer}/vue-scaffold/`, {
+  //     statusCode: 401
+  //   }).as('vueScaffold')
+  //
+  //   cy.visit(`${baseUrl}/login`)
+  //   cy.login()
+  //
+  //   cy.location('pathname').should('include', '/login')
+  // })
 })
